ddcd4af13b92d1cbe683bb212745ace6f7a609d9
galina <galina.ramodina@gmail.com>
2023-12-28 23:42:12 +0300

    написаны обычные тесты

--- src/test/java/hw13/calculator/CalculatorTest.java
+++ src/test/java/hw13/calculator/CalculatorTest.java
@@ -1,9 +1,23 @@
 package hw13.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+//public static class CalculatorServiceTestConstants{
+//    public final int num1 = 1;
+//    public int num2 = 2;
+//
+//}
+//public static Stream<Arguments> provideParamsForTest() {
+//    return Stream.of(
+//            Arguments.of(1,2)
+//    );
+//}
 public class CalculatorTest {
     private final CalculatorService calculatorService = new CalculatorService();
     private final int num1=1;
@@ -40,4 +54,9 @@ public class CalculatorTest {
         double actual = (double) num1 /num2;
         assertEquals(expected,actual);
     }
+//    @ParameterizedTest
+//    @MethodSource("provideParamsForTest")
+//    public void shouldGenerateCorrectMessage (int num1, int num2){
+//
+//    }
 }
