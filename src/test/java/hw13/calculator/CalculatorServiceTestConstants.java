7153b0037f87aef9d363f72e55b745f89c385e60
galina <galina.ramodina@gmail.com>
2024-01-05 18:58:35 +0300

    +ParameterizedTest

---
+++ src/test/java/hw13/calculator/CalculatorServiceTestConstants.java
@@ -0,0 +1,7 @@
+package hw13.calculator;
+
+public class CalculatorServiceTestConstants{
+    public final int num1 = 1;
+    public int num2 = 2;
+
+}
--- src/test/java/hw13/calculator/CalculatorTest.java
+++ src/test/java/hw13/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package hw13.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,17 +9,36 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//public static class CalculatorServiceTestConstants{
-//    public final int num1 = 1;
-//    public int num2 = 2;
-//
-//}
-//public static Stream<Arguments> provideParamsForTest() {
-//    return Stream.of(
-//            Arguments.of(1,2)
-//    );
-//}
+
 public class CalculatorTest {
+    public static Stream<Arguments> provideParamsForTestPlus() {
+        return Stream.of(
+            Arguments.of(1,2, 3),
+            Arguments.of(5,6,11),
+                Arguments.of(3,5,8)
+        );
+    }
+    public static Stream<Arguments> provideParamsForTestMinus() {
+        return Stream.of(
+                Arguments.of(1,2, -1),
+                Arguments.of(5,6,-1),
+                Arguments.of(5,3,2)
+        );
+    }
+    public static Stream<Arguments> provideParamsForTestMultiply() {
+        return Stream.of(
+                Arguments.of(1,2,2),
+                Arguments.of(5,6,30),
+                Arguments.of(5,3,15)
+        );
+    }
+    public static Stream<Arguments> provideParamsForTestDivide() {
+        return Stream.of(
+                Arguments.of(2,1,2),
+                Arguments.of(5,5,1),
+                Arguments.of(6,3,2)
+        );
+    }
     private final CalculatorService calculatorService = new CalculatorService();
     private final int num1=1;
     private int num2=2;
@@ -54,9 +74,28 @@ public class CalculatorTest {
         double actual = (double) num1 /num2;
         assertEquals(expected,actual);
     }
-//    @ParameterizedTest
-//    @MethodSource("provideParamsForTest")
-//    public void shouldGenerateCorrectMessage (int num1, int num2){
-//
-//    }
+    @ParameterizedTest
+    @MethodSource("provideParamsForTestPlus")
+    public void plusParametrized (int num1, int num2, int plus){
+        int result = calculatorService.plus(num1, num2);
+        Assertions.assertEquals(plus, result);
+    }
+    @ParameterizedTest
+    @MethodSource("provideParamsForTestMinus")
+    public void minusParametrized (int num1, int num2, int minus){
+        int result = calculatorService.minus(num1, num2);
+        Assertions.assertEquals(minus, result);
+    }
+    @ParameterizedTest
+    @MethodSource("provideParamsForTestMultiply")
+    public void multiplyParametrized (int num1, int num2, int multiply){
+        int result = calculatorService.multiply(num1, num2);
+        Assertions.assertEquals(multiply, result);
+    }
+    @ParameterizedTest
+    @MethodSource("provideParamsForTestDivide")
+    public void divideParametrized (int num1, int num2, int divide){
+        double result = calculatorService.divide(num1, num2);
+        Assertions.assertEquals(divide, result);
+    }
 }
